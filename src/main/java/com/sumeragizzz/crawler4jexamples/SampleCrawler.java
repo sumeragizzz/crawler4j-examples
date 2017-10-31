00668994debc3434704ebf47fc3c8cac6c0e72bd
sumeragizzz <forester@cocoa.plala.or.jp>
2017-11-01 00:12:54 +0900

    add jsoup sample code

--- src/main/java/com/sumeragizzz/crawler4jexamples/SampleCrawler.java
+++ src/main/java/com/sumeragizzz/crawler4jexamples/SampleCrawler.java
@@ -1,5 +1,9 @@
 package com.sumeragizzz.crawler4jexamples;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
@@ -18,13 +22,11 @@ public class SampleCrawler extends WebCrawler {
         System.out.format("**** %s - %s ****%n", page.getWebURL().getURL(), page.getRedirectedToUrl());
 
         HtmlParseData parseData = cast(page.getParseData());
-        parseData.getHtml();
 
-        System.out.println("--------");
-        System.out.println(parseData.getHtml());
+        Document document = Jsoup.parse(parseData.getHtml());
 
-        System.out.println("--------");
-        System.out.println(parseData.getText());
+        Elements acGnList = document.getElementsByClass("ac-gn-list");
+        acGnList.forEach(e -> System.out.println(e.select("li a span").text()));
     }
 
     @SuppressWarnings("unchecked")
